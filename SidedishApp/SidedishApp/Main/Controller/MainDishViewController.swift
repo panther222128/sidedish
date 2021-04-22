ec73ea402e0df62e8c9823adae7c10ee55e52273
Lia <judykim257@gmail.com>
2021-04-22 13:25:08 +0900

    feat: collectionView 골격 형성 - DiffableDataSource 활용

--- SidedishApp/SidedishApp/Main/Controller/MainDishViewController.swift
+++ SidedishApp/SidedishApp/Main/Controller/MainDishViewController.swift
@@ -8,36 +8,82 @@
 import UIKit
 
 class MainDishViewController: UIViewController {
-
-    enum Section: Int, CaseIterable {
-        case mainDish = 0, hotSoup, sideDish
-    }
     
     @IBOutlet weak var dishCollectionView: UICollectionView!
     
-    
     private var dishDataSource: UICollectionViewDiffableDataSource<Section, ProductModel>!
-    
-    
+    private var snapshot = NSDiffableDataSourceSnapshot<Section, ProductModel>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        registerNib()
+        configureDataSource()
+        createSnapshot()
     }
-    
-
-
+      
 }
 
 extension MainDishViewController {
     
+    enum Section: Int, CaseIterable {
+        case mainDish = 0, hotSoup, sideDish
+    }
+    
     typealias DishType = ProductModel
     typealias SectionType = Section
     
-    func configureDataSource() {
-        self.dishDataSource = UICollectionViewDiffableDataSource<SectionType, DishType>(collectionView: dishCollectionView, cellProvider: { (collectionView, indexPath, dish) -> UICollectionViewCell? in
-            guard let cell = dishCollectionView.dequeueReusableCell(withReuseIdentifier: <#T##String#>, for: <#T##IndexPath#>) else { retrun  }
-        })
+    private func registerNib() {
+        let nibName = UINib(nibName: DishCell.reuseIdentifier, bundle: nil)
+        dishCollectionView.register(nibName, forCellWithReuseIdentifier: DishCell.reuseIdentifier)
+    }
+    
+    private func configureDataSource() {
+        self.dishDataSource = UICollectionViewDiffableDataSource<SectionType, DishType>(collectionView: dishCollectionView) { (collectionView, indexPath, dish) -> DishCell? in
+            
+            guard let cell = self.dishCollectionView.dequeueReusableCell(withReuseIdentifier: DishCell.reuseIdentifier, for: indexPath) as? DishCell else { return DishCell()  }
+            print(cell)
+            print("d")
+            
+            cell.imageView.image = dish.image
+            cell.nameLabel.text = dish.name
+            cell.descriptionLabel.text = dish.description
+            cell.priceLabel.text = dish.price
+            cell.discountedPriceLabel.text = dish.discountedPrice
+            cell.eventBadgeLabel.text = dish.eventBadge
+            
+            return cell
+        }
+    }
+    
+    private func createSnapshot() {
+        self.snapshot.appendSections([.mainDish])
+        self.snapshot.appendItems(DishManager.setDish())
+        self.dishDataSource.apply(snapshot, animatingDifferences: false)
+    }
+}
+
+
+
+
+class DishManager {
+    typealias Dish = ProductModel
+    
+    private var dishes: [Dish]
+    
+    init(dishes: [Dish]) {
+        self.dishes = dishes
+    }
+    
+    static func setDish() -> [Dish] {
+        var dishes = [Dish]()
+        
+        let data = try! Data(contentsOf: URL(string: "https://user-images.githubusercontent.com/73650994/115364788-ba0b4d00-a1fe-11eb-8066-7ed2c620d43e.jpeg")!)
+        guard let image = UIImage(data: data) else {return []}
+        
+        let dish = Dish(image: image, name: "쿼카", description: "졸귀", price: "100억", discountedPrice: "그딴 거 없다", eventBadge: "초특가")
+        
+        dishes.append(dish)
+        
+        return dishes
     }
 }
