5b658a821cbf57fdadb39def65060d59e8232846
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-04-23 10:57:36 +0900

    feat: 상세 화면 테스트를 위한 임의의 값을 갖는 ViewModel 생성

---
+++ SidedishApp/SidedishApp/ProductDetail/ViewModel/OrderQuantity.swift
@@ -0,0 +1,14 @@
+//
+//  OrderQuantityModel.swift
+//  SidedishApp
+//
+//  Created by Jun Ho JANG on 2021/04/21.
+//
+
+import Foundation
+
+struct OrderQuantity {
+    
+    let quantity = "수량"
+    
+}
---
+++ SidedishApp/SidedishApp/ProductDetail/ViewModel/ProductDescriptionImage.swift
@@ -0,0 +1,14 @@
+//
+//  ProductDescriptionImageModel.swift
+//  SidedishApp
+//
+//  Created by Jun Ho JANG on 2021/04/21.
+//
+
+import UIKit
+
+struct ProductDescriptionImage {
+    
+    let descriptionImageName = ".jpg"
+    
+}
