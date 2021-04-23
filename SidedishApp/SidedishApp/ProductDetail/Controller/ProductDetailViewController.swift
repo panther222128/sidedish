697d2e4e8c66b5ea8c3d975d9a081814e8a4d051
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-04-23 10:58:43 +0900

    feat: Controller, View와 ViewModel을 갖고 있고 ViewModel의 값을 View에 Configure

---
+++ SidedishApp/SidedishApp/ProductDetail/Controller/ProductDetailViewController.swift
@@ -0,0 +1,95 @@
+//
+//  ProductDetailViewController.swift
+//  SidedishApp
+//
+//  Created by Jun Ho JANG on 2021/04/20.
+//
+
+import UIKit
+
+class ProductDetailViewController: UIViewController {
+    
+    @IBOutlet weak var productDetailView: ProductDetailView!
+    
+    private let productDetail = ProductDetail()
+    private let deliveryCondition = DeliveryCondition()
+    private let orderQuantity = OrderQuantity()
+    private let orderInfo = OrderInfo()
+    private let productDescriptionImage = ProductDescriptionImage()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureProductImage()
+        configureProductDetail()
+        configureDeliveryCondition()
+        configureOrderQuantity()
+        configureOrderInfo()
+        configureProductDetailDescription()
+    }
+    
+    func configureProductImage() {
+        var contentWidth = CGFloat(0.0)
+        if let containerView = productDetailView.containerView {
+            for image in 0...3 {
+                let imageToDisplay = UIImage(named: "무야호.jpg")
+                let imageView = UIImageView(image: imageToDisplay)
+                let xCoordinate = containerView.frame.midX + containerView.frame.width * CGFloat(image)
+                let yCoordinate = containerView.frame.minY
+                containerView.productImage.addSubview(imageView)
+                imageView.contentMode = .scaleAspectFit
+                imageView.frame = CGRect(x: xCoordinate - containerView.productImage.frame.width / 2, y: yCoordinate, width: containerView.productImage.frame.width, height: containerView.productImage.frame.height)
+                contentWidth += containerView.productImage.frame.width
+            }
+            containerView.productImage.contentSize = CGSize(width: contentWidth, height: containerView.productImage.frame.height)
+        }
+    }
+    
+    func configureProductDetail() {
+        if let containerView = productDetailView.containerView {
+            containerView.productDetailContainer.eventBadge.text = productDetail.productDetailData[0].eventBadge
+            containerView.productDetailContainer.eventBadge.clipsToBounds = true
+            containerView.productDetailContainer.eventBadge.layer.cornerRadius = 5
+        }
+    }
+    
+    func configureDeliveryCondition() {
+        if let containerView = productDetailView.containerView {
+            containerView.deliveryConditionContainer.reserve.text = deliveryCondition.reserve
+            containerView.deliveryConditionContainer.reserveAmount.text = deliveryCondition.reserveAmount
+            containerView.deliveryConditionContainer.deliveryInfo.text = deliveryCondition.deliveryInfo
+            containerView.deliveryConditionContainer.deliveryInfoDetail.text = deliveryCondition.deliveryInfoDetail
+            containerView.deliveryConditionContainer.deliveryCharge.text = deliveryCondition.deliveryCharge
+            containerView.deliveryConditionContainer.deliveryChargeAmount.text = deliveryCondition.deliveryChargeAmount
+        }
+    }
+    
+    func configureOrderQuantity() {
+        if let containerView = productDetailView.containerView {
+            containerView.orderQuantityContainer.quantity.text = orderQuantity.quantity
+        }
+    }
+    
+    func configureOrderInfo() {
+        if let containerView = productDetailView.containerView {
+            containerView.orderInfoContainer.totalAmountTitle.text = orderInfo.order
+            containerView.orderInfoContainer.orderButton.titleLabel?.text = orderInfo.order
+            containerView.orderInfoContainer.orderButton.layer.cornerRadius = 5
+        }
+    }
+    
+    func configureProductDetailDescription() {
+        if let containerView = productDetailView.containerView {
+            let imageToDisplay = UIImage(named: "a.jpg")
+            let imageView = UIImageView(image: imageToDisplay)
+            let xCoordinate = containerView.productDescription.frame.minX
+            let yCoordinate = containerView.productDescription.frame.minY
+            imageView.contentMode = .scaleAspectFit
+            if let image = UIImage(named: "a.jpg") {
+                let imageHeight = image.size.height
+                imageView.frame = CGRect(x: xCoordinate, y: yCoordinate, width: containerView.productDescription.frame.width, height: imageHeight)
+            }
+            containerView.productDescription.image = imageToDisplay
+        }
+    }
+    
+}
