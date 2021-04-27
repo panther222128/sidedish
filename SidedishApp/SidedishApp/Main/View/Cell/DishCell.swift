229cc100cb1b44e3515968049ed680d9bbb7c9ac
Lia <judykim257@gmail.com>
2021-04-27 17:37:06 +0900

    feat: cell image 추가

--- SidedishApp/SidedishApp/Main/View/Cell/DishCell.swift
+++ SidedishApp/SidedishApp/Main/View/Cell/DishCell.swift
@@ -10,6 +10,8 @@ import UIKit
 class DishCell: UICollectionViewCell {
 
     static let reuseIdentifier = "DishCell"
+
+    static let nib : UINib = UINib(nibName: DishCell.reuseIdentifier, bundle: nil)
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
@@ -23,9 +25,29 @@ class DishCell: UICollectionViewCell {
     }
     
     func fill(with product: Dish) {
+        DispatchQueue.main.async {
+            self.convert(imageUrlString: product.image) { uiimage in
+                self.imageView.image = uiimage
+            }
+        }
+        
         self.nameLabel.text = product.title
         self.descriptionLabel.text = product.description
         self.priceLabel.text = product.salePrice
+        self.discountedPriceLabel.text = product.normalPrice ?? ""
+        guard product.eventBadge?.count != 0, let eventBadge = product.eventBadge?[0] else {
+            return
+        }
+        self.eventBadgeLabel.text = eventBadge
+    }
+    
+    private func convert(imageUrlString: String, handler: @escaping (UIImage) ->()) {
+        guard let url = URL(string: imageUrlString) else { return }
+        DispatchQueue.global().async {
+            let data = try? Data(contentsOf: url)
+        print("이미지 로드")
+         handler(UIImage(data: data!) ?? UIImage())
+        }
     }
 
 }
