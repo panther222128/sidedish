af2ee342be27a2401399c4e1198bae2ecc6970ff
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-04-27 17:56:34 +0900

    feat: View 구조 개성늘 위한 ContainerView 추가

--- SidedishApp/SidedishApp/ProductDetail/View/ContainerView.swift
+++ SidedishApp/SidedishApp/ProductDetail/View/ContainerView.swift
@@ -10,11 +10,15 @@ import UIKit
 class ContainerView: UIView {
     
     @IBOutlet weak var productImage: UIScrollView!
+    @IBOutlet weak var productImages: UIView!
     @IBOutlet weak var productDetailContainer: ProductDetailContainer!
     @IBOutlet weak var deliveryConditionContainer: DeliveryConditionContainer!
     @IBOutlet weak var orderQuantityContainer: OrderQuantityContainer!
     @IBOutlet weak var orderInfoContainer: OrderInfoContainer!
+    @IBOutlet weak var detailImageStackView: UIStackView!
     @IBOutlet weak var productDescription: UIImageView!
+    @IBOutlet weak var productDesxcription1: UIImageView!
+    @IBOutlet weak var productDesxcription2: UIImageView!
 
 }
 
