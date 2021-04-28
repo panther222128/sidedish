9dd33a70dac99e6ba1a48af2fb378b42b8cb2852
Lia <judykim257@gmail.com>
2021-04-28 18:40:05 +0900

    refactor: Section enum 파일 분리

---
+++ SidedishApp/SidedishApp/Main/Model/DishSection.swift
@@ -0,0 +1,14 @@
+//
+//  DishSection.swift
+//  SidedishApp
+//
+//  Created by Lia on 2021/04/28.
+//
+
+import Foundation
+
+enum Section: String, CaseIterable {
+    case main = "한그릇 뚝딱 메인 요리"
+    case soup = "김이 모락모락 국.찌개"
+    case side = "언제 먹어도 든든한 밑반찬"
+}
