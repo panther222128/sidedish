9149e73d7aa8c0812aa1b573b612e145442bc853
Lia <judykim257@gmail.com>
2021-04-28 18:50:26 +0900

    fix: network api 맞게 model 타입 변경

---
+++ SidedishApp/SidedishApp/Model/DetailDish.swift
@@ -0,0 +1,37 @@
+//
+//  DetailDish.swift
+//  SidedishApp
+//
+//  Created by Lia on 2021/04/28.
+//
+
+import Foundation
+
+struct DetailDishes: Decodable {
+    var hash: String
+    var data: [DetailDish]
+}
+
+struct DetailDish: Hashable, Codable {
+    let uuid = UUID()
+    
+    let topImage: String
+    let thumbImage: [String]
+    let productDescription: String
+    let point: String
+    let deliveryInfo: String
+    let deliveryFee: String
+    let price: [String]
+    let detailSection: [String]
+    
+    enum CodingKeys: String, CodingKey {
+        case topImage = "top_image"
+        case thumbImage = "thumb_image"
+        case productDescription = "product_description"
+        case point
+        case deliveryInfo = "delivery_info"
+        case deliveryFee = "delivery_fee"
+        case price
+        case detailSection = "detail_section"
+    }
+}
